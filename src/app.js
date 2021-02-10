27bcbc330d789dfe2e4ea520cbbc48979f904ec7
alexandr <santichechecheck@gmail.com>
2021-02-10 14:12:57 +0200

    crs

--- src/app.js
+++ src/app.js
@@ -52,6 +52,12 @@ app.options(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
